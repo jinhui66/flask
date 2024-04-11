052032382d5a34db38aac6817ae0a4aee96a2949
jinhui66 <1303101041@qq.com>
2024-04-11 21:52:31 +0800

    all

--- api.py
+++ api.py
@@ -1,15 +1,15 @@
-import openai
+# import openai
 
-# bp = Blueprint('api',__name__,url_prefix="/")
+# # bp = Blueprint('api',__name__,url_prefix="/")
 
-# @bp.route('/chat_api',methods=['GET','POST'])
-def chat_api(question):
-    key = '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'
-    openai.api_key = key
-    response = openai.completions.create(
-        model='text-davinci-003',
-        prompt=rf'{question}'
-    )
-    return response.choice[0].text
+# # @bp.route('/chat_api',methods=['GET','POST'])
+# def chat_api(question):
+#     key = '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'
+#     openai.api_key = key
+#     response = openai.completions.create(
+#         model='text-davinci-003',
+#         prompt=rf'{question}'
+#     )
+#     return response.choice[0].text
     
-print(chat_api('很好'))
+# print(chat_api('很好'))
